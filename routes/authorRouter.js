e7446eb460247a922e236be7973fcda68abfc3eb
Jeremyh007 <jinglu.huang@student.unimelb.edu.au>
2021-04-22 21:22:40 +0800

    initial commit

---
+++ routes/authorRouter.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+// add our router
+const authorRouter = express.Router()
+
+//require the quthor controller
+const authorController = require('../controllers/authorController.js')
+
+
+//handle the GET request to get all authors
+authorRouter.get('/',authorController.getAllAuthors)
+
+// handle the GET request to get one author
+authorRouter.get('/:id', authorController.getAuthorByID)
+
+// handle POST requests to add one author
+authorRouter.post('/', authorController.addAuthor)
+
+//export the router
+module.exports = authorRouter
